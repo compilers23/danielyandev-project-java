f2d53053d9c0081fcdfbd646020077a7fca5c663
Ruben Danielyan <rub1994.13@gmail.com>
2023-11-28 13:19:58 +0400

    Create parser

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
+import parser.Parser;
 import tokenizer.Tokenizer;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String forthCode = "5 3 * dup + . cr";
+//        String forthCode = "5 3 * dup + . cr";
+        String forthCode = "3 4 * dup nip 5 - swap tuck 2 / drop over + . cr";
         Tokenizer tokenizer = new Tokenizer();
         List<String> tokens = tokenizer.tokenize(forthCode);
 
-        // Display the tokens
-        for (String token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        parser.parse();
+
+//        // Display the tokens
+//        for (String token : tokens) {
+//            System.out.println(token);
+//        }
+
     }
 }
